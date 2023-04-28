577b95ef3ac6631013ff3e8f90efa99695a69ac6
Korzen <kubako2000@gmail.com>
2023-04-28 16:56:52 +0200

    removed double tags from comments for code clarity

--- main.py
+++ main.py
@@ -24,22 +24,22 @@ with ytdlp:
 
 print('Audio extracted successfully!')
 
-# # Set up the recognizer object
+# Set up the recognizer object
 r = sr.Recognizer()
-#
-# # Load the audio file
+
+# Load the audio file
 audio_file = sr.AudioFile('C:/Users\kubak\PycharmProjects\pythonProject1/mysong.wav')
-#
-# # Open the audio file and convert to audio data
+
+# Open the audio file and convert to audio data
 with audio_file as source:
     audio_data = r.record(source)
-#
-# # Use the recognize_google() method to convert audio to text
+
+# Use the recognize_google() method to convert audio to text
 recognized_text = r.recognize_google(audio_data, language='pl-PL')
-#
-# # Count the number of occurrences of "yyy"
+
+# Count the number of occurrences of "yyy"
 yyy_count = recognized_text.count('odkurzacza')
-#
-# # Print the result
+
+# Print the result
 print(f"Found {yyy_count} occurrences of 'odkurzacza'.")
 print(recognized_text)
