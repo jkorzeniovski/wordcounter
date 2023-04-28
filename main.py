12ca920ca472e2307fa30d1ec7e6cd01b092e452
Korzen <kubako2000@gmail.com>
2023-04-28 15:59:36 +0200

    added yt_dlp to download and analyze youtube video

--- main.py
+++ main.py
@@ -1,20 +1,46 @@
 import speech_recognition as sr
+import yt_dlp
 
-# Set up the recognizer object
+# Create a yt-dlp options object
+ytdlp_options = {
+    'format': 'bestaudio/best',  # Choose the best audio format available
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'wav',  # Convert the audio to wav
+        'preferredquality': '192',  # Set the audio quality
+    }],
+    'outtmpl': 'mysong.mp3'  # Specify the output file name
+}
+# --ffmpeg-location:C\Users\kubak\AppData\Local\Microsoft\WinGet\Packages\Gyan.FFmpeg_Microsoft.Winget.Source_8wekyb3d8bbwe\ffmpeg-6.0-full_build\bin
+
+# Create a yt-dlp object with the options
+ytdlp = yt_dlp.YoutubeDL(ytdlp_options)
+
+# URL of the video to extract audio from
+video_url = 'https://www.youtube.com/watch?v=Bs4GfrBiYYc'
+
+# Use yt-dlp to extract audio from the video
+with ytdlp:
+    result = ytdlp.extract_info(video_url, download=True)
+
+print('Audio extracted successfully!')
+
+# # Set up the recognizer object
 r = sr.Recognizer()
-
-# Load the audio file
-audio_file = sr.AudioFile('C:/Users\kubak\OneDrive\Pulpit/yyy6.wav')
-
-# Open the audio file and convert to audio data
+#
+# # Load the audio file
+audio_file = sr.AudioFile('C:/Users\kubak\PycharmProjects\pythonProject1/mysong.wav')
+#
+# # Open the audio file and convert to audio data
 with audio_file as source:
     audio_data = r.record(source)
-
-# Use the recognize_google() method to convert audio to text
+#
+# # Use the recognize_google() method to convert audio to text
 recognized_text = r.recognize_google(audio_data, language='pl-PL')
-
-# Count the number of occurrences of "yyy"
+#
+# # Count the number of occurrences of "yyy"
 yyy_count = recognized_text.count('obraz')
-
-# Print the result
+#
+# # Print the result
 print(f"Found {yyy_count} occurrences of 'obraz'.")
+print(recognized_text)
